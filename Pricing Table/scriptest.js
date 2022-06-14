52e3801e5bea2eff16be2a9121aba891b3d3d93c
GabrielRJn <gabrielrainajohnson@Gmail.com>
2022-06-14 14:26:08 +0100

    Add project files.

---
+++ Pricing Table/scriptest.js
@@ -0,0 +1,18 @@
+window.onload = writeMessage();
+
+function writeMessage(){
+    document.getElementById("Subheading").innerHTML
+    = "Welcome to the website.";
+    
+}
+
+function initAll() {
+
+    document.getElementById("redirect"),
+     onclick = initRedirect;
+}
+
+function initRedirect() {
+    window.location = "kiwi.html"
+    return false;
+}
